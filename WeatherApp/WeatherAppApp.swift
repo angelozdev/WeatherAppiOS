5e0b8f07dcb3f36426d21f5b4135f5d73e771984
Angelo Zambrano <angelozam17@gmail.com>
2024-11-23 10:18:16 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Angelo Zambrano on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
